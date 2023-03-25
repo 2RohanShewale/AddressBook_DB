156254343aa0ecff3144bc20d4b7f9f959cbf1fc
Rohan Shewale <shewalerohan50@gmail.com>
2023-03-25 08:01:08 +0530

    [Rohan]Creating Table

--- AddressBookSQL.sql
+++ AddressBookSQL.sql
@@ -1,4 +1,16 @@
 --UC1 Creating Database
 CREATE DATABASE AddressBook_Service
 
-
+--UC2 Creating  Address Book table 
+CREATE TABLE AddressBook_DB
+(
+PersonId INT PRIMARY KEY IDENTITY(1,1),
+FirstName VARCHAR(30) NOT NULL,
+LastName VARCHAR(30) NOT NULL,
+Address VARCHAR(100) NOT NULL,
+City VARCHAR(20) NOT NULL,
+State VARCHAR(20) NOT NULL,
+Zip INT NOT NULL,
+PhoneNumber BIGINT NOT NULL,
+Email VARCHAR(50) NOT NULL
+);
