ef409ab0757f3974505777fd850d5d0350b52af9
Rohan Shewale <shewalerohan50@gmail.com>
2023-03-25 08:25:59 +0530

    [Rohan]Insering values

--- AddressBookSQL.sql
+++ AddressBookSQL.sql
@@ -14,3 +14,13 @@ Zip INT NOT NULL,
 PhoneNumber BIGINT NOT NULL,
 Email VARCHAR(50) NOT NULL
 );
+
+--UC3
+INSERT INTO AddressBook_DB(FirstName,LastName,Address,City,State,Zip,PhoneNumber,Email)
+VALUES
+('Rohan','Shewale','location','Mumbai','Maharastra',400022,1234567890,'rohan@gamil.com'),
+('Mandar','Bhurke','location','Thane','Maharastra',400022,1234567890,'mandar@gamil.com'),
+('Shubham','Wagh','location','Pune','Maharastra',400022,1234567890,'wagh@gamil.com'),
+('Chhaya','Shewale','location','Pune','Maharastra',400022,1234567890,'chhaya@gamil.com'),
+('Priya','Bhurke','location','Thane','Maharastra',400022,1234567890,'priya@gamil.com'),
+('Nikita','Wagh','location','Pune','Maharastra',400022,1234567890,'nikita@gamil.com')
